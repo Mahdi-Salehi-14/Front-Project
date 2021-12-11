b3ddcb3bf025db0ec32538764bb6b8e16393b6c0
Mahdi-Salehi-14 <Mahdi2015GAD@Gmail.com>
2021-12-11 19:45:40 +0330

    11 Dec 2021

--- ProductList_View.php
+++ ProductList_View.php
@@ -10,25 +10,30 @@
             }
         </style>
     </head>
-    <body>
-        <h1>کاتالوگ محصولات</h1>
-        <h2>محصولات</h2>
-        <div class = "grid-container">
-            <?php
-                foreach($productlist as $product)
-                    {
-                        echo "
-                            <article class = 'card'>
-                                <img src = '{$product['productimg']}'>
-                                <br>
-                                <div class = 'card-body'>
-                                    <h3><a href = 'ProductDetails.php?id={$product['id']}'>{$product['productname']}</a></h3>
-                                    <p>قیمت :{$product['productprice']}</p>
-                                </div>
-                            </article>
-                        ";
-                    }
-            ?>
-        </div>
+    <body class = "body-grid">
+        <header>Header</header>
+        <aside>Aside</aside>
+        <main>
+            <h1>کاتالوگ محصولات</h1>
+            <h2>محصولات</h2>
+            <div class = "grid-container">
+                <?php
+                    foreach($productlist as $product)
+                        {
+                            echo "
+                                <article class = 'card'>
+                                    <img src = '{$product['productimg']}'>
+                                    <br>
+                                    <div class = 'card-body'>
+                                        <h3><a href = 'ProductDetails.php?id={$product['id']}'>{$product['productname']}</a></h3>
+                                        <p>قیمت :{$product['productprice']}</p>
+                                    </div>
+                                </article>
+                            ";
+                        }
+                ?>
+            </div>
+        </main>
+        <footer>Footer</footer>
     </body>
 </html>
